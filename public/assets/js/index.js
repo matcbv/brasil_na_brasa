d5ab62319dc026f1d754cfc148cf0dbf8324c757
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-10-25 18:59:17 -0300

    Adicao da nova secao da pagina inicial

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const carousel = document.querySelector('.recipes-carousel');
+const toRigth = document.querySelector('.right-span');
+const toLeft = document.querySelector('.left-span');
+
+toRigth.addEventListener('click', () => {
+    const emphasisItem = carousel.querySelector('.emphasis');
+    const nextItem = emphasisItem.nextElementSibling.nodeName !== "DIV" ? emphasisItem.nextElementSibling.nextElementSibling: emphasisItem.nextElementSibling;
+    nextItem.classList.add('emphasis');
+    emphasisItem.classList.remove('emphasis');
+    carousel.appendChild(carousel.removeChild(emphasisItem));
+});
