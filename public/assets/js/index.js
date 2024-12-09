0fe110d003d07a71e26496e8cf160e07ce70274f
matcbv <matheuscbv23@gmail.com>
2024-12-09 01:57:52 -0300

    Adicao do inicio da logica para o carrossel

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,31 +1,3 @@
-const carousel = document.querySelector('.recipes-carousel');
-const toRigth = document.querySelector('.right-span');
-const toLeft = document.querySelector('.left-span');
-const menuBtn = document.querySelector('.menu-btn');
-
-toRigth.addEventListener('click', () => {
-    const emphasisItem = carousel.querySelector('.emphasis');
-    const nextItem = emphasisItem.nextElementSibling.nodeName !== "DIV" ? emphasisItem.nextElementSibling.nextElementSibling: emphasisItem.nextElementSibling;
-    nextItem.classList.add('emphasis');
-    emphasisItem.classList.remove('emphasis');
-    carousel.appendChild(carousel.removeChild(emphasisItem));
-});
-
-// Evento para ativar prompt do prato do dia
-menuBtn.addEventListener('click', () => {
-    const nome = prompt("Digite seu nome:");
-
-    const pratos = ["Feijoada", "Lasanha", "Strogonoff", "Macarrão", "Pizza", "Sushi", "Frango"];
-
-    const diaSemana = new Date().getDay();
-
-    const pratoDoDia = pratos[diaSemana];
-
-    const mensagem = `Olá, ${nome}! O prato do dia é ${pratoDoDia}!`;
-
-    alert(mensagem);
-})
-
 // ----------- Header Animations -----------
 
 const header = document.querySelector('header');
@@ -38,3 +10,23 @@ document.addEventListener('scroll', () => {
         header.classList.add('animations');
     };
 });
+
+// ----------- Main Animations -----------
+
+const toLeft = document.querySelector('.left-span');
+const toRight = document.querySelector('.right-span');
+const carouselContainer = document.querySelector('.recipes-carousel')
+
+toLeft.addEventListener('click', () => {
+    const mainDish = document.querySelector('.emphasis');
+    const nextDish = mainDish.previousElementSibling;
+    mainDish.classList.remove('emphasis');
+    nextDish.classList.add('emphasis');
+})
+
+toRight.addEventListener('click', () => {
+    const mainDish = document.querySelector('.emphasis');
+    const nextDish = mainDish.nextElementSibling;
+    mainDish.classList.remove('emphasis');
+    nextDish.classList.add('emphasis');
+})
