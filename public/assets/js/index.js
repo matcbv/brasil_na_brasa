c4b57279ed1da317d97da9b5064531064afc3737
matcbv <matheuscbv23@gmail.com>
2024-12-10 14:12:18 -0300

    Adicao de detalhes no js do carousel

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -19,8 +19,29 @@ const items = document.querySelectorAll('.item');
 let currentItem = 0;
 const maxItem = items.length;
 
-arrows.forEach(arrow, () => {
-   arrow.addEventListener('click', () => {
-      
-   })
-})
+arrows.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+        console.log('opa');
+        if(arrow.classList.contains('left-span')){
+            currentItem -= 1;
+        } else{
+            currentItem += 1;
+        };
+
+        if(currentItem >= maxItem){
+            currentItem = 0;
+        } else if(currentItem < 0){
+            console.log('entreiii')
+            currentItem = maxItem - 1;
+        }
+
+        items.forEach((item) => {
+            item.classList.remove('emphasis');
+        });
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        })
+    });
+});
