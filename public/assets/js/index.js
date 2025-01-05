af481be96c6382aa95936a6f36bc9f5231083244
matcbv <matheuscbv23@gmail.com>
2025-01-05 01:30:23 -0300

    Finalizacao do projeto

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,17 @@
+const header = document.querySelector('header');
+const carousel = document.querySelector('.recipes');
+let items = document.querySelectorAll('.item');
+const popup = document.querySelector('.popup');
+const checkout = document.querySelector('.checkout');
+const totalSpan = document.querySelector('.total-value');
+const itemsContainer = document.querySelector('.items-container');
+const cartContainer = document.querySelector('.cart-container');
+const counterSpan = document.querySelector('.counter');
+
 // ----------- Hero Banner Button -----------
 
-const bannerBtn = document.querySelector('.menu-btn');
-const recipesSection = document.querySelector('.recipes-section');
-
-bannerBtn.addEventListener('click', () => {
-    recipesSection.scrollIntoView({
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.recipes-section').scrollIntoView({
         behavior: "smooth",
         block: "center",
         inline: "center"
@@ -12,8 +19,6 @@ bannerBtn.addEventListener('click', () => {
 })
 
 // ----------- Header Animation -----------
-
-const header = document.querySelector('header');
 
 document.addEventListener('scroll', () => {
     if(window.scrollY === 0){
@@ -26,15 +31,12 @@ document.addEventListener('scroll', () => {
 
 // ----------- Carousel Animation -----------
 
-const carousel = document.querySelector('.recipes');
-const arrows = document.querySelectorAll('.arrow');
-let items = document.querySelectorAll('.item');
-
-arrows.forEach((arrow) => {
+document.querySelectorAll('.arrow').forEach((arrow) => {
     arrow.addEventListener('click', () => {
 
         items.forEach((item) => {
             item.classList.remove('emphasis');
+
         });
 
         arrow.classList.contains('left-span') ? carousel.prepend(carousel.lastElementChild): carousel.appendChild(carousel.firstElementChild);
@@ -45,167 +47,158 @@ arrows.forEach((arrow) => {
 
 // ----------- Shopping Cart Animation -----------
 
-const popup = document.querySelector('.popup');
-const buttons = document.querySelectorAll('.button');
-const cartIcon = document.querySelector('.cart-icon');
-const closeIcon = document.querySelector('.close-icon');
-const checkout = document.querySelector('.checkout');
-const totalSpan = document.querySelector('.total-value');
-const itemsContainer = document.querySelector('.items-container');
-const cartContainer = document.querySelector('.cart-container');
+class Cart{
+    constructor(){
+        this.cartItem = 
+        `<p class="recipe-name"></p>
+        <span class="spacement"> - </span>
+        <p class="recipe-price"></p>
+        <div class="quantity-container">
+            <img class="quantity-icon remove-icon" src="../public/assets/images/popup/remove_icon.png" alt="Remover">
+            <span class="item-count"></span>
+            <img class="quantity-icon add-icon" src="../public/assets/images/popup/add_icon.png" alt="Adicionar">
+        </div>`;
+        
+        this.cartIndex = 0;
+        this.totalValue = 0;
+        this.recipesInCart = [];
+    };
 
-const cartItem = 
-`<p class="recipe-name"></p>
-<span class="spacement"> - </span>
-<p class="recipe-price"></p>
-<div class="quantity-container">
-    <img class="quantity-icon remove-icon" src="../public/assets/images/popup/remove_icon.png" alt="Remover">
-    <span class="item-count"></span>
-    <img class="quantity-icon add-icon" src="../public/assets/images/popup/add_icon.png" alt="Adicionar">
-</div>`
-
-let index = 0;
-
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        index += 1;
-        const counterSpan = document.querySelector('.counter')
+    incrementCounter(){
+        this.cartIndex += 1;
         if(!cartContainer.contains(counterSpan)){
             const counterSpan = document.createElement('span');
             counterSpan.classList.add('counter');
-            counterSpan.innerHTML = index;
+            counterSpan.innerHTML = this.cartIndex;
             cartContainer.prepend(counterSpan);
         } else{
-            counterSpan.innerHTML = index;
+            counterSpan.innerHTML = this.cartIndex;
         };
-        
-        const recipesInCart = sessionStorage.getItem('recipesInCart');
-        const recipeContainer = btn.parentElement;
-        const recipeName = recipeContainer.querySelector('h3').innerText;
-        const recipeValue = recipeContainer.querySelector('.price').innerText;
+    };
 
-        if(recipesInCart){
-            let recipesArr = JSON.parse(recipesInCart);
-            
-            if(recipesArr.some(obj => obj.name === recipeName)){
-                recipesArr.forEach(obj => {
-                    if(obj.name === recipeName){
-                        obj.index += 1;
+    addIconEvents(item){
+        const quantityIcons = item.querySelectorAll('.quantity-icon');
+        const itemCount = item.querySelector('.item-count');
+    
+        quantityIcons.forEach(icon => {
+            icon.addEventListener('click', () => {
+
+                const recipeName = item.querySelector('.recipe-name');
+
+                newItem.recipesInCart.forEach(obj => {
+                    if(obj.name === recipeName.innerHTML){
+                        if(icon.classList.contains('remove-icon')){
+                            obj.index -= 1;
+                            itemCount.innerHTML = parseInt(itemCount.innerHTML) - 1;
+                        }else{
+                            obj.index += 1;
+                            itemCount.innerHTML = parseInt(itemCount.innerHTML) + 1;
+                        };
+                        if (obj.index <= 0){
+                            item.remove();
+                            const index = newItem.recipesInCart.indexOf(obj);
+                            newItem.recipesInCart.splice(index, 1);
+                            this.isEmpty();
+                        };
                     };
                 });
-            } else{
-                recipesArr.push({name: recipeName, value: recipeValue, index: 1});
-            };
+                this.getTotalValue();
+                sessionStorage.setItem('recipesInCart', JSON.stringify(newItem.recipesInCart));
+            });
+        });
+    };
 
-            sessionStorage.setItem('recipesInCart', JSON.stringify(recipesArr));
+    getTotalValue(){
+        this.totalValue = 0;
+        for(const i of newItem.recipesInCart){
+            this.totalValue += i.index * parseFloat(i.value.slice(3,));
+        }
+        totalSpan.innerHTML = `${this.totalValue.toFixed(2)}`;
+    };
+
+    showRecipesInCart(){
+        itemsContainer.innerHTML = '';
+        popup.style.display = "flex";
+
+        if(newItem.recipesInCart.length > 0){
+            newItem.recipesInCart.forEach(obj => {
+                const {name, value, index} = obj;
+                const item = document.createElement('div');
+                item.classList.add('item');
+                item.innerHTML = this.cartItem;
+                item.querySelector('.recipe-name').innerHTML = name;
+                item.querySelector('.recipe-price').innerHTML = value;
+                item.querySelector('.item-count').innerHTML = index;
+                itemsContainer.appendChild(item);
+
+                this.addIconEvents(item);
+    
+                checkout.disabled = false;
+                checkout.classList.remove('disabled');
+                this.getTotalValue();
+            });
+        }else{
+            this.isEmpty();
+        };
+    };
+
+    isEmpty(){
+        if(newItem.recipesInCart.length <= 0){
+            const p = document.createElement('p');
+            p.innerHTML = 'Ops... Seu carrinho está vazio!';
+            itemsContainer.innerHTML = '';
+            itemsContainer.appendChild(p);
+            checkout.disabled = true;
+            checkout.classList.add('disabled');
+            totalSpan.innerHTML = '00,00';
+        }
+    }
+}
+
+const newItem = new Cart();
+
+window.addEventListener('load', () => {
+    const recipesInStorage = JSON.parse(sessionStorage.getItem('recipesInCart'));
+    if(recipesInStorage){
+        newItem.recipesInCart = recipesInStorage;
+    };
+});
+
+document.querySelectorAll('.button').forEach(btn => {
+    btn.addEventListener('click', () => {
+        newItem.incrementCounter();
+
+        const recipeContainer = btn.parentElement;
+        const containerName = recipeContainer.querySelector('h3').innerText;
+        const containerValue = recipeContainer.querySelector('.price').innerText;
+
+        if(newItem.recipesInCart.length > 0 && newItem.recipesInCart.some(item => item.name === containerName)){
+            for(const item of newItem.recipesInCart){
+                if(item.name === containerName){
+                    item.index += 1;
+                };
+            };
+            sessionStorage.setItem('recipesInCart', JSON.stringify(newItem.recipesInCart));
         } else{
-            const recipesJSON = JSON.stringify([{name: recipeName, value: recipeValue, index: 1}]);
-            sessionStorage.setItem('recipesInCart', recipesJSON);
+            const itemArr = {name: containerName, value: containerValue, index: 1};
+            newItem.recipesInCart.push(itemArr);
+            sessionStorage.setItem('recipesInCart', JSON.stringify(itemArr));
         };
     });
 });
 
-cartIcon.addEventListener('click', () => {
-    addRecipeInCart();
-    popup.style.display = "flex";
-});
+document.querySelector('.cart-icon').addEventListener('click', () => newItem.showRecipesInCart());
 
-closeIcon.addEventListener('click', () => popup.style.display = "none" );
-
-function addRecipeInCart(){
-    const recipesInCart = sessionStorage.getItem('recipesInCart');
-    if(recipesInCart){
-        let totalValue = 0;
-        const recipesArr = JSON.parse(recipesInCart)
-        itemsContainer.innerHTML = '';
-        recipesArr.forEach(obj => {
-            const {name, value, index} = obj;
-            if(index > 0){
-                const item = document.createElement('div');
-                item.classList.add('item');
-                item.innerHTML = cartItem;
-                item.querySelector('.recipe-name').innerHTML = name;
-                item.querySelector('.recipe-price').innerHTML = value;
-                item.querySelector('.item-count').innerHTML = index;
-                
-                itemsContainer.appendChild(item);
-    
-                addIconEvents(item, recipesArr)
-    
-                const valueToSum = (Array.from(value).slice(3, )).join('').replace(',', '.');
-                totalValue += parseInt(index, 10) * Number.parseFloat(valueToSum);
-                checkout.disabled = false;
-            }
-        });
-        totalSpan.innerHTML = `R$ ${totalValue.toFixed(2)}`;
-    } else{
-        const p = document.createElement('p');
-        p.innerHTML = 'Ops... Seu carrinho está vazio!';
-        itemsContainer.innerHTML = '';
-        itemsContainer.appendChild(p);
-        checkout.disabled = true;
-        totalSpan.innerHTML = `R$ 00,00`;
-    }
-};
-
-function addIconEvents(item, recipesInCart){
-    const quantityIcons = item.querySelectorAll('.quantity-icon');
-    const itemCount = item.querySelector('.item-count');
-
-    quantityIcons.forEach(icon => {
-        if(icon.classList.contains('remove-icon')){
-            icon.addEventListener('mouseover', () => {
-                itemCount.style.color = '#fb5858';
-            });
-
-            icon.addEventListener('click', () => {
-                const recipeName = item.querySelector('.recipe-name');
-                recipesInCart.forEach(obj => {
-                    if(obj.name === recipeName.innerHTML){
-                        obj.index -= 1;
-                        if (obj.index <= 0){
-                            item.remove();
-                        }
-                        itemCount.innerHTML = parseInt(itemCount.innerHTML) - 1;
-                    };
-                });
-                sessionStorage.setItem('recipesInCart', JSON.stringify(recipesInCart));
-            });
-        }else{
-            icon.addEventListener('mouseover', () => {
-                itemCount.style.color = '#6bbb6e';
-            });
-
-            icon.addEventListener('click', () => {
-                const recipeName = item.querySelector('.recipe-name');
-                recipesInCart.forEach(obj => {
-                    if(obj.name === recipeName.innerHTML){
-                        obj.index += 1;
-                        if (obj.index <= 0){
-                            item.remove();
-                        }
-                        itemCount.innerHTML = parseInt(itemCount.innerHTML) + 1;
-                    };
-                });
-                sessionStorage.setItem('recipesInCart', JSON.stringify(recipesInCart));
-            });
-        }
-
-        icon.addEventListener('mouseleave', () => {
-            itemCount.style.color = 'white';
-        });
-    })
-}
+document.querySelector('.close-icon').addEventListener('click', () => popup.style.display = "none");
 
 checkout.addEventListener('click', () => {
     popup.style.display = 'none';
     sessionStorage.setItem('recipesInCart', '');
-    const counter = document.querySelector('.counter');
-    counter.remove();
-    index = 0;
+    document.querySelector('.counter').remove();
+    newItem.cartIndex = 0;
 
     const successMsg = document.querySelector('p');
-    successMsg.innerHTML = 'Compra finalizada com sucesso!'
+    successMsg.innerHTML = 'Compra finalizada com sucesso!';
     const successPopup = document.createElement('div');
     successPopup.appendChild(successMsg);
     successPopup.classList.add('success-div');
@@ -213,7 +206,5 @@ checkout.addEventListener('click', () => {
 
     setTimeout(() => {
         successPopup.remove();
-    }, 3000)
-})
-
-
+    }, 3000);
+});
